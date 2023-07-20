4a672e9546e1ba5c9814f6cd41f9e43b31f00f5e
Samir1009ms <yusifovs1009@gmail.com>
2023-07-20 21:54:51 +0400

    complated

--- src/components/Overwiev/OverwievLeft.jsx
+++ src/components/Overwiev/OverwievLeft.jsx
@@ -8,25 +8,20 @@ export default function OverwievLeft() {
     const loading = useSelector((state) => state.transactionsSlice.loading);
     const { t } = useTranslation()
 
-    function click() {
-        console.log(window);
-    }
     return (
         <div className={`${style.balansCont} p-3  col-4`}>
-
             <div className={`${style.balans}  `}>
                 {/* //! total balans */}
                 <span className={`${style.total}`}>{t('balance')}</span>
                 <span className={`${style.amout}`}>$ {total.toLocaleString('en-US')}</span>
 
             </div>
-            {/* <Skeleton width="100%" height="150px" background="red" style={{ background: "red !important", color: "red" }}></Skeleton> */}
             <div className={`${style.balans}`}>
                 {/* //! credit limit */}
                 <span className={`${style.total}`}>{t('credit')}</span>
                 <span className={`${style.limit}`}>$ 1,743</span>
             </div>
-            <button onClick={click} className={`${style.paymentBtn}`}>{t('payment')}</button>
+            <button className={`${style.paymentBtn}`}>{t('payment')}</button>
         </div>
     )
 }
