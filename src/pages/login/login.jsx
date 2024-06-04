653e07c1c766d64a831466e3eebbd4c2ebeeef04
Samir1009ms <yusifovs1009@gmail.com>
2024-06-04 12:17:38 +0400

    update

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -29,11 +29,13 @@ const Login = () => {
 
     useEffect(() => {
         let theme = localStorage.getItem("theme");
-        if (theme === "dark") {
-            document.body.classList.add("dark");
+        if (theme === "light") {
+            document.body.classList.add("light");
         } else {
-            document.body.classList.remove("dark");
-            localStorage.setItem("theme", "light")
+            document.body.classList.remove("light")
+            document.body.classList.add("dark");
+
+            localStorage.setItem("theme", "dark")
         }
         const selectedLanguage = localStorage.getItem('lang');
         if (selectedLanguage) {
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -60,13 +60,13 @@ export function Register() {
 
     useEffect(() => {
         let theme = localStorage.getItem("theme");
-        if (theme === "dark") {
-            document.body.classList.add("dark");
-            // setTheme("dark");
+        if (theme === "light") {
+            document.body.classList.add("light");
         } else {
-            document.body.classList.remove("dark");
-            localStorage.setItem("theme", "light")
-            // setTheme("light");
+            document.body.classList.remove("light");
+            document.body.classList.add("dark");
+
+            localStorage.setItem("theme", "dark")
         }
         const selectedLanguage = localStorage.getItem('lang');
         if (selectedLanguage) {
